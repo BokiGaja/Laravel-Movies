0eb05693da4422ca04cdda5babaf4b7f60803708
BokiGaja <bgajic93@gmail.com>
2019-02-12 21:27:51 +0100

    third commit - Fv2

--- app/Http/Controllers/GenresController.php
+++ app/Http/Controllers/GenresController.php
@@ -10,6 +10,7 @@ class GenresController extends Controller
     public function show($genre)
     {
         $movies = Movie::where('genre', 'LIKE', $genre)->get();
-        return view('movies.index', compact('movies'));
+        $sideMovies = Movie::orderBy('id', 'desc')->take(5)->get();
+        return view('movies.index', compact('movies', 'sideMovies'));
     }
 }
--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,12 +1,11 @@
-
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-            <div class="sidebar-sticky">
-                <ul class="nav flex-column">
-                    @foreach($sideMovies as $movie)
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/movies/{{ $movie->id }}">{{ $movie->title }}</a>
-                    </li>
-                    @endforeach
-                </ul>
-            </div>
-        </nav>
+<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+    <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+            @foreach($movies as $movie)
+                <li class="nav-item">
+                    <a class="nav-link active" href="/movies/{{ $movie->id }}">{{ $movie->title }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</nav>
